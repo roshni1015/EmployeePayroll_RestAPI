ed8047561818105ad4a3cc34b198e59b9676bff3
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-24 19:47:13 +0530

    [Roshni]-Retrieve Employee Payroll Data

---
+++ EmployeePayroll_RestAPI/EmployeePayroll_RestAPI/RestSharpe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_RestAPI
+{
+    public class RestSharpe
+    {
+        public class Employee
+        {
+            public int id { get; set; }
+            public string Name { get; set; }
+            public string Salary { get; set; }
+
+
+        }
+        
+    }
+}
